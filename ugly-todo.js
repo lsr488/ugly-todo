60e0e7c92fadf01a775b65134ad6db6ab01ec944
Lisa <lsr488@gmail.com>
2019-02-13 23:27:10 -0500

    Removed superfluous showTodos methods.

--- ugly-todo.js
+++ ugly-todo.js
@@ -1,40 +1,20 @@
 let todoList = {
 	todos: [],
-	showTodos: function() {
-		if(this.todos.length === 0) {
-			console.log("You have no tasks.");
-		} else {
-			console.log("My Todo List:");
-			for(var i = 0; i < this.todos.length; i++) {
-				// shows whether a task has been completed or not
-				if(this.todos[i].completed === true) {
-					var taskCompleted = "(x)";
-				} else {
-					taskCompleted = "( )";
-				}
-				console.log(taskCompleted + " " + this.todos[i].text);
-			}
-		}
-	},
 	addItem: function(todoText) {
 		this.todos.push({
 				text: todoText,
 				completed: false
 			});
-		this.showTodos();
 	},
 	editItem: function(index, todoText) {
 		this.todos[index].text = todoText;
-		this.showTodos();
 	},
 	delItem: function(index) {
 		this.todos.splice(index, 1);
-		this.showTodos();
 	},
 	toggleCompleted: function(index) {
 		let todo = this.todos[index];
 		todo.completed = !todo.completed;
-		this.showTodos();
 	},
 	toggleAll: function() {
 		var completedTodos = 0;
@@ -56,7 +36,6 @@ let todoList = {
 				this.todos[i].completed = true;
 			}			
 		}
-		this.showTodos();
 	}
 }
 
@@ -65,16 +44,15 @@ var handlers = {
 	clearInputs: function(field) {
 		field.value = "";
 	},
-	showTodos: function() {
-		todoList.showTodos();
-	},
 	toggleAll: function() {
 		todoList.toggleAll();
+		view.showTodos();
 	},
 	addItem: function() {
 		var addTodoTextInput = document.getElementById("add-todo-text-input");
 		todoList.addItem(addTodoTextInput.value);
 		this.clearInputs(addTodoTextInput);
+		view.showTodos();
 	},
 	editItem: function() {
 		var changeTodoTextInput = document.getElementById("change-todo-text-input");
@@ -82,16 +60,19 @@ var handlers = {
 		todoList.editItem(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
 		this.clearInputs(changeTodoPositionInput);
 		this.clearInputs(changeTodoTextInput);
+		view.showTodos();
 	},
 	delItem: function() {
 		var deleteTodoPositionInput = document.getElementById("delete-todo-position-input");
 		todoList.delItem(deleteTodoPositionInput.valueAsNumber);
 		this.clearInputs(deleteTodoPositionInput);
+		view.showTodos();
 	},
 	toggleCompleted: function() {
 		var toggleCompletedPositionInput = document.getElementById("toggle-completed-position-input");
 		todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
 		this.clearInputs(toggleCompletedPositionInput);
+		view.showTodos();
 	}
 }
 
